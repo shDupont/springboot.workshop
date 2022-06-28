a38d905cdc46f255c6d14346d4821536eaf59ed7
Letícia Dupont <leticia.dupont20@gmail.com>
2022-06-28 20:33:29 -0300

    Exception handling - delete

--- src/main/java/com/workshop/dupont/services/UserServices.java
+++ src/main/java/com/workshop/dupont/services/UserServices.java
@@ -2,8 +2,11 @@ package com.workshop.dupont.services;
 
 import com.workshop.dupont.entities.User;
 import com.workshop.dupont.repositories.UserRepository;
+import com.workshop.dupont.services.exceptions.DatabaseException;
 import com.workshop.dupont.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,7 +31,14 @@ public class UserServices {
     }
 
     public void delete(Long id){
-        userRepository.deleteById(id);
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+
     }
 
     public User update(Long id, User obj){
---
+++ src/main/java/com/workshop/dupont/services/exceptions/DatabaseException.java
@@ -0,0 +1,7 @@
+package com.workshop.dupont.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+    public DatabaseException(String msg){
+        super(msg);
+    }
+}
