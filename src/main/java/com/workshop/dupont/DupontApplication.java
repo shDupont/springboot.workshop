23e4437bf0e094bd3b172d84fc00b085cda49258
Letícia Dupont <leticia.dupont20@gmail.com>
2022-06-28 14:38:18 -0300

    Initial commit

---
+++ src/main/java/com/workshop/dupont/DupontApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.dupont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DupontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DupontApplication.class, args);
+	}
+
+}
